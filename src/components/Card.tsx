2fd7aef53e941051ef06d196aedfb1153257b0d8
evertonvargas <evertonpassa7@gmail.com>
2021-11-24 01:58:07 -0300

    implementação do delete

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,7 @@
 import { format } from 'date-fns';
 import { Activitie } from "../types/types";
+import { supabase } from "../services/supabase";
+import { useActivities } from "../hooks/useActivities";
 
 import styles from "./../styles/components/card.module.scss";
 
@@ -7,7 +9,16 @@ interface CardProps extends Omit<Activitie, "subject_id"> {
   page: boolean;
 }
 
-export const Card = ({ deadLine, link, description, page }: CardProps) => {
+export const Card = ({ deadLine, link, description, page, id }: CardProps) => {
+  const { activitiesData, setActivitiesData } = useActivities();
+
+  async function handleDelete(id: string){
+    const { data } = await supabase.from('Activities').delete().match({ id })
+
+    const activitieFiltered = activitiesData.filter( activitie => activitie.id !== id)
+    setActivitiesData(activitieFiltered);
+  }
+
   return (
     <div className={styles.card}>
       {page ? (
@@ -30,11 +41,7 @@ export const Card = ({ deadLine, link, description, page }: CardProps) => {
             </span>
           </div>
           <div>
-            <button type="button" className="icon">
-              <img src="pen.svg" alt="lixeira" />
-            </button>
-
-            <button type="button" className="icon">
+            <button type="button" className="icon" onClick={()=>handleDelete(id)}>
               <img src="garbage.svg" alt="lixeira" />
             </button>
           </div>
